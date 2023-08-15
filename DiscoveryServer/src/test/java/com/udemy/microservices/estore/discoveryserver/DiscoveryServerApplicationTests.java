fed647022141d085113cdd909f6928db6cad6eda
Timur Celikel <timur.celikel@proton.me>
2023-08-15 11:07:54 -0700

    initial Eureka Discovery Server commit

---
+++ DiscoveryServer/src/test/java/com/udemy/microservices/estore/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.udemy.microservices.estore.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
---
+++ ProductsService/src/main/java/com/udemy/microservices/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,28 @@
+package com.udemy.microservices.estore.ProductsService.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+	@PostMapping
+	public String createProduct() {
+		return "HTTP POST Handled";
+	}
+
+	@GetMapping
+	public String getProduct() {
+		return "HTTP GET Handled";
+	}
+
+	@PutMapping
+	public String updateProduct() {
+		return "HTTP PUT Handled";
+	}
+
+	@DeleteMapping
+	public String deleteProduct() {
+		return "HTTP DELETE Handled";
+	}
+}

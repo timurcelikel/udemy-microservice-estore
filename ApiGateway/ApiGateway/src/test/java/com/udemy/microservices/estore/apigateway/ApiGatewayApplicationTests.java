c579f56092277c020566de0a6680a4b67ea0273d
Timur Celikel <timur.celikel@proton.me>
2023-08-20 13:38:52 -0700

    Added API Gateway configuration for multiple running products microservice instances

---
+++ ApiGateway/ApiGateway/src/test/java/com/udemy/microservices/estore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.udemy.microservices.estore.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
--- ProductsService/src/main/java/com/udemy/microservices/estore/ProductsService/rest/ProductsController.java
+++ ProductsService/src/main/java/com/udemy/microservices/estore/ProductsService/rest/ProductsController.java
@@ -1,10 +1,21 @@
 package com.udemy.microservices.estore.ProductsService.rest;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+	//@Autowired -- IntelliJ warning says use Constructor Injection instead
+	//private Environment env;
+
+	private final Environment env;
+
+	public ProductsController(final Environment env) {
+
+		this.env = env;
+	}
 
 	@PostMapping
 	public String createProduct() {
@@ -15,7 +26,7 @@ public class ProductsController {
 	@GetMapping
 	public String getProduct() {
 
-		return "HTTP GET Handled";
+		return "HTTP GET Handled " + env.getProperty("local.server.port");
 	}
 
 	@PutMapping
